422711955b146d9928a93b3f0439fdd0140f9086
katya sitko <kat.sitko@gmail.com>
2023-02-26 17:21:53 +0100

    added new gallery js

--- script.js
+++ script.js
@@ -28,39 +28,85 @@ likeBtn.addEventListener("click", function () {
   document.querySelector(".count-number").textContent = increaseCounter;
 });
 
+// CAROUSEL
+// The ul element ith all our imgs
+const track = document.querySelector('.carousel-track-container');
 
-// const next = document.getElementById('next');
-// const prev = document.getElementById("prev");
-// const images = document.querySelector(".carousel-container");
+// Select the slides as an array
+const slides = Array.from(track.children);
+console.log(track)
 
-// let currentCard = 0;
+// When I click left btn, move slides to left
+const leftButton = document.querySelector('.carousel-button--left');
 
-// next.addEventListener("click", () => {
-//   if (currentCard < images.children.length -1)
-// {currentCard++;
-//   images.style.transitionDuration = "0.9s";
-// images.style.transform = `translate(-${currentCard * 640}px)`;
-// }
-// else {
-//   return;
-// }
-// });
+// When I click right btn, move slides to right
+const rightButton = document.querySelector('.carousel-button--right');
 
-// prev.addEventListener("click", () => {
-//   if (currentCard > 0)
-// {currentCard--;
-//   images.style.transitionDuration = "0.9s";
-// images.style.transform = `translate(-${currentCard * 640}px)`;
-// }
-// else {
-//   return;
-// }
-// });
-var counterNav = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 6){
-        counter = 1;
-      }
-    }, 5000);
+// When I click on nav btns, move to that slide
+const dotsNav = document.querySelector('.carousel-nav');
+
+// Select all nav dots
+const dots = Array.from(dotsNav.children);
+console.log(dotsNav.children)
+
+// Working with the width of the carousel-track
+const slideWidth = slides[0].getBoundingClientRect().width;
+console.log(slideWidth)
+
+// Arrange slides next to each other instead of being stacked
+// slides[0].style.left = 0;
+// slides[1].style.left = slideWidth + "px";
+// ^^ this moved the second slide by the width of the slide
+
+// Write loop for above and write function
+const setSlidePosition = (slide, index) => {slide.style.left = slideWidth * index + 'px';};
+slides.forEach(setSlidePosition);
+
+//move slide to the right
+// rightButton.addEventListener('click', e => {
+//   const currentSlide = track.querySelector('.current-slide');
+//   currentSlide.classList.remove('current-slide');
+//   const nextSlide = currentSlide.nextElementSibling;
+//   nextSlide.classList.add('current-slide')
+//   const amountToMove = nextSlide.style.left;
+//   track.style.transform = 'translateX(-'+amountToMove+')';
+// })
+
+// function for writing the code above for either left or right btns
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  // the slide we click on will gain the class, doesn't have to be next or previous
+  targetSlide.classList.add('current-slide')
+}
+
+rightButton.addEventListener('click', e => {
+const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+moveToSlide(track, currentSlide, nextSlide);
+});
+
+leftButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+  moveToSlide(track, currentSlide, prevSlide);
+  });
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+}
+
+
+  dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides [targetIndex];
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+  })
